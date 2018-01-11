0544630940b85a679afd5922f831c7ba697af905
Jamie <jamie.lo112@gmail.com>
2018-01-10 20:16:03 -0800

    [add]
    Minor styling and accounts draw condition.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './app.css';
 
 class App extends Component {
   constructor() {
@@ -6,6 +7,8 @@ class App extends Component {
     this.state = {
       player: 1,
       mark: 'X',
+      winner: false,
+      count: 0,
       1: '1',
       2: '2',
       3: '3',
@@ -17,10 +20,30 @@ class App extends Component {
       9: '9',
     }
 
+    this.getInitialState = this.getInitialState.bind(this);
     this.handleInput = this.handleInput.bind(this);
     this.playerTurn = this.playerTurn.bind(this);
     this.markBoard = this.markBoard.bind(this);
     this.winCondition = this.winCondition.bind(this);
+    this.clearBoard = this.clearBoard.bind(this);
+  }
+
+  getInitialState() {
+    return {
+      player: 1,
+      mark: 'X',
+      winner: false,
+      count: 0,
+      1: '1',
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+    }
   }
 
   handleInput(e) {
@@ -30,8 +53,6 @@ class App extends Component {
     this.setState({
       [name]: value,
     })
-
-    console.log(this.state.position)
   }
 
   playerTurn() {
@@ -49,13 +70,13 @@ class App extends Component {
   }
 
   markBoard(value) {
-    console.log('im clicking', value)
-
     if (this.state[value] === 'X' || this.state[value] === 'O') {
       alert(`${value} is already marked pick another location`);
     } else {
+
       this.setState({
-        [value]: this.state.mark
+        [value]: this.state.mark,
+        count: this.state.count+1,
       })
       this.playerTurn();
     }
@@ -63,36 +84,89 @@ class App extends Component {
   }
 
   winCondition() {
-    console.log('win?', this.state[1], this.state[2], this.state[3])
     if (this.state[1] === this.state.mark && this.state[2] === this.state.mark && this.state[3] === this.state.mark ) {
-      alert(`PLAYER ${this.state.player} WON!`)
+      this.setState({
+        winner: true
+      })
     }
+    if (this.state[4] === this.state.mark && this.state[5] === this.state.mark && this.state[6] === this.state.mark ) {
+      this.setState({
+        winner: true
+      })
+    }
+    if (this.state[7] === this.state.mark && this.state[8] === this.state.mark && this.state[9] === this.state.mark ) {
+      this.setState({
+        winner: true
+      })
+    }
+    if (this.state[1] === this.state.mark && this.state[4] === this.state.mark && this.state[7] === this.state.mark ) {
+      this.setState({
+        winner: true
+      })
+    }
+    if (this.state[2] === this.state.mark && this.state[5] === this.state.mark && this.state[8] === this.state.mark ) {
+      this.setState({
+        winner: true
+      })
+    }
+    if (this.state[3] === this.state.mark && this.state[6] === this.state.mark && this.state[9] === this.state.mark ) {
+      this.setState({
+        winner: true
+      })
+    }
+    if (this.state[1] === this.state.mark && this.state[5] === this.state.mark && this.state[9] === this.state.mark ) {
+      this.setState({
+        winner: true
+      })
+    }
+    if (this.state[3] === this.state.mark && this.state[5] === this.state.mark && this.state[7] === this.state.mark ) {
+      this.setState({
+        winner: true
+      })
+    }
+  }
+
+  clearBoard() {
+    this.setState(this.getInitialState());
   }
 
 
   render() {
     return (
       <div className="center">
-        <h1>Let's Play Tic Tac Toe</h1>
+        <h1 className="top-vh-md">Let's Play Tic Tac Toe</h1>
         <div>Player {this.state.player}'s turn, click on grid to place {this.state.mark}</div>
 
-        <div className="container">
+        {this.state.winner ? (<div>Player {this.state.player} WON!</div>) : null}
+        {this.state.count === 9 ? (<div>DRAW!</div>) : null}
+
+        <div className="container top-vh-lg">
           <div className="row">
-            <div className="col-md-4" onClick={() => {this.markBoard('1')}}>{this.state[1]}</div>
-            <div className="col-md-4" onClick={() => {this.markBoard('2')}}>{this.state[2]}</div>
-            <div className="col-md-4" onClick={() => {this.markBoard('3')}}>{this.state[3]}</div>
-          </div>
-          <div className="row">
-            <div className="col-md-4" onClick={() => {this.markBoard('4')}}>{this.state[4]}</div>
-            <div className="col-md-4" onClick={() => {this.markBoard('5')}}>{this.state[5]}</div>
-            <div className="col-md-4" onClick={() => {this.markBoard('6')}}>{this.state[6]}</div>
-          </div>
-          <div className="row">
-            <div className="col-md-4" onClick={() => {this.markBoard('7')}}>{this.state[7]}</div>
-            <div className="col-md-4" onClick={() => {this.markBoard('8')}}>{this.state[8]}</div>
-            <div className="col-md-4" onClick={() => {this.markBoard('9')}}>{this.state[9]}</div>
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <div className="row">
+                <div className="col-md-4" onClick={() => {this.markBoard('1')}}>{this.state[1]}</div>
+                <div className="col-md-4" onClick={() => {this.markBoard('2')}}>{this.state[2]}</div>
+                <div className="col-md-4" onClick={() => {this.markBoard('3')}}>{this.state[3]}</div>
+              </div>
+              <div className="row">
+                <div className="col-md-4" onClick={() => {this.markBoard('4')}}>{this.state[4]}</div>
+                <div className="col-md-4" onClick={() => {this.markBoard('5')}}>{this.state[5]}</div>
+                <div className="col-md-4" onClick={() => {this.markBoard('6')}}>{this.state[6]}</div>
+              </div>
+              <div className="row">
+                <div className="col-md-4" onClick={() => {this.markBoard('7')}}>{this.state[7]}</div>
+                <div className="col-md-4" onClick={() => {this.markBoard('8')}}>{this.state[8]}</div>
+                <div className="col-md-4" onClick={() => {this.markBoard('9')}}>{this.state[9]}</div>
+              </div>
+            </div>
+            <div className="col-md-4"></div>
           </div>
         </div>
+
+        {this.state.winner ? (<button type="button" className="btn btn-primary" onClick={this.clearBoard}>Clear</button>) : null}
+        {this.state.count === 9 ? (<button type="button" className="btn btn-primary" onClick={this.clearBoard}>Clear</button>) : null}
+
       </div>
     );
   }
